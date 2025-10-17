960b65d270006e580b1112595c41e801ef428599
Vishnu-BB <kurravishnu.blubridge@evoplus.in>
2025-10-17 16:06:54 +0530

    Merge main into restore-careful-deletion

---
+++ checkpoint.cpp
@@ -0,0 +1,54 @@
+[33mbd2e1fb[m[33m ([m[1;36mHEAD[m[33m -> [m[1;32mmain[m[33m, [m[1;31morigin/main[m[33m, [m[1;31morigin/HEAD[m[33m)[m CPU_fwd_bwd_Kernels
+[33mf58a824[m Merge branch 'mg1503:main' into main
+[33m80a2cc5[m Update README.md
+[33m581c0a5[m Update README.md
+[33m3ed88e9[m Update README.md
+[33m39fdb23[m Update README.md
+[33m723f153[m Update README.md
+[33m3a8ab9d[m inplace correction
+[33me44a60c[m topo_from
+[33m2944ddb[m Merge branch 'main' of https://github.com/Vishnu-BB/cgadimpl
+[33me744955[m checkpoint and careful deletion
+[33mde25626[m Merge branch 'restore-careful-deletion'
+[33mcdaa8be[m[33m ([m[1;31morigin/restore-careful-deletion[m[33m, [m[1;32mrestore-careful-deletion[m[33m)[m Reapply "Merge pull request #8 from Vishnu-BB/main"
+[33m6cd6c9a[m Merge pull request #1 from mg1503/main
+[33m001b0e9[m[33m ([m[1;31mgrishma/main[m[33m, [m[1;31mgrishma/HEAD[m[33m)[m kernel update
+[33m0ace20d[m Revert "Merge pull request #8 from Vishnu-BB/main"
+[33m3506616[m Merge pull request #8 from Vishnu-BB/main
+[33m321629c[m[33m ([m[1;33mtag: [m[1;33mv0.1-careful_deletion[m[33m)[m feat(careful deletion): add gradient-careful deletion baseline
+[33m2377589[m[33m ([m[1;33mtag: [m[1;33mv0.2-checkpointing[m[33m)[m feat(checkpoint): add gradient-checkpointing baseline
+[33m9b53593[m CMake file change
+[33m3764e62[m Merge branch 'main' into main
+[33mce73df7[m nabil: nodeops update
+[33m8a86e3d[m Merge branch 'mg1503:main' into main
+[33me40270a[m new feature: no manual linkage of lib, its taken care by code!
+[33m9f37fc3[m Merge branch 'main' into main
+[33me755859[m checkpoint update
+[33m9f34b19[m checkpoint verified
+[33mc8c482d[m Merge branch 'main' into main
+[33mb180eea[m Clean up build artifacts and apply .gitignore
+[33m426346f[m kernel update
+[33mc056fd1[m delete
+[33m45c498e[m Checkpointing
+[33m0e28e0d[m kernel folder added
+[33m9722709[m run update
+[33m59a4a57[m changes done by nabil
+[33md79461d[m changes done by nabil
+[33m51dcedc[m merging the updates made by nabil abdulla
+[33m454b202[m[33m ([m[1;31mgrishma/trial[m[33m)[m g
+[33mfbe23c1[m g
+[33m89606f8[m v4
+[33mbc39223[m g
+[33mb4195f0[m s
+[33ma617954[m s
+[33m8bc93aa[m s
+[33ma612dfc[m s
+[33madbcf21[m v4
+[33mebb1f06[m ntg much
+[33m957d559[m makefile updates
+[33mbc94c66[m with stable hlir update
+[33mc10d1a3[m v2
+[33ma0a9920[m learning
+[33m223a749[m learning
+[33mf88feca[m first commit
+[33m92c4490[m first commit
